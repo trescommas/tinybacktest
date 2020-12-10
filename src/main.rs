3c34b74319a767fe982a4b09250276bbd8f19ffc
Abhisheak Mall <abhisheak@ariscg.com>
2020-12-10 23:55:59 +0800

    added strategy class

--- src/main.rs
+++ src/main.rs
@@ -41,11 +41,37 @@ impl DataFrame{
 	
 }
 
+struct Strategy{
+
+
+}
+
+impl Strategy {
+    
+    fn new() -> Strategy{
+        
+    }
+    
+    /// The main backtest loop
+    ///
+    fn backtest(&mut self, df: &DataFrame) -> f64 {
+        
+        for (i, date) in df.time.iter().enumerate() {
+            continue;
+        }
+
+        return 42.0;
+    }
+
+}
+
+
+
 
 fn main() {
     let start = Instant::now();
 	let df: DataFrame = DataFrame::load_csv("/Users/abi/tinybacktest/src/trainTHETA-PERP.csv"); 
     let duration = start.elapsed();
-
+    backtest(&df);
     println!("Time taken to load csv file {:?}", duration);
 }
